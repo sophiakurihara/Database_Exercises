e204b36152675d795775f871b9a17e2b4813e20d
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-04 09:19:28 -0500

    finished TABLES exercise for sql.

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,13 @@
 USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums (
+	id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+	artist VARCHAR(50) NOT NULL,
+	name VARCHAR(100) DEFAULT 'NONE',
+	release_date DATE,
+	sales INT UNSIGNED,
+	genre VARCHAR(100) DEFAULT 'NONE',
+	PRIMARY KEY (id)
+);
