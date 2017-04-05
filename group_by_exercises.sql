1905252c10d05a14c2b9e6bddf57207ae5af339f
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-05 11:35:29 -0500

    finished group_by exercises

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -6,3 +6,13 @@ SELECT last_name
 FROM employees
 WHERE last_name LIKE 'E%e'
 GROUP BY last_name;
+
+SELECT first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%e'
+GROUP BY first_name, last_name;
+
+SELECT last_name FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
