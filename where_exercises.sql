173383ea71e935dbfc172e2222fcf0d529c99765
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-05 10:48:41 -0500

    added .gitignore

---
+++ where_exercises.sql
@@ -0,0 +1,21 @@
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE gender = 'M' 
+AND (
+	first_name IN ('Irena', 'Vidya') 
+	OR first_name = 'Maya'
+	);
+-- Finds all male employees with these first names
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT * FROM employees
+WHERE hire_date BETWEEN '1990-01-01' and '1999-12-31';
+
+SELECT * FROM employees
+WHERE birth_date LIKE '%-12-25';
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%';
