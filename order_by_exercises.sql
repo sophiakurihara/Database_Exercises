397289710f01e7983d5a50e644fbc105d19af28d
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-05 11:14:44 -0500

    finished last exercises. Khun Bernini

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -28,7 +28,8 @@ WHERE last_name LIKE '%q%';
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC;
 -- finds all employees hired in the 90's with birthdays on christmas
 
 SELECT * FROM employees
