46357b33f1d7406bc4e0fd109364b710e23f7660
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-05 11:13:26 -0500

    reversed order of queries using DESC

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -5,14 +5,14 @@ AND (
 	first_name IN ('Irena', 'Vidya') 
 	OR first_name = 'Maya'
 	)
-ORDER BY first_name, last_name;
+ORDER BY first_name DESC, last_name DESC;
 -- Finds all male employees with these first names
 -- in ascending order by first name and last name
 
 SELECT emp_no, first_name, last_name
 FROM employees
 WHERE last_name LIKE 'E%e'
-ORDER BY emp_no;
+ORDER BY emp_no DESC;
 -- employees whose last names begin and end with 'e'
 
 SELECT * FROM employees
