04dd04f49feea55ebd23f93db992cacdb0ca9919
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-05 12:16:23 -0500

    finished limit_exercises. learned OFFSET as well

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -2,3 +2,11 @@ SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date, hire_date DESC
+LIMIT 5 OFFSET 45;
+
