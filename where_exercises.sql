bc2933abd6c42f0028cdb6d30c887a4cb527537d
Sophia Sudiacal <sophia.sudiacal@gmail.com>
2017-04-05 10:55:41 -0500

    added new query

--- where_exercises.sql
+++ where_exercises.sql
@@ -19,3 +19,9 @@ WHERE birth_date LIKE '%-12-25';
 
 SELECT * FROM employees
 WHERE last_name LIKE '%q%';
+
+SELECT emp_no, first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25';
+-- finds all employees hired in the 90's with birthdays on christmas
